04dae483bd633e5e130ac7e201d049c84bcabed2
James Rock <hello@jamesrock.me>
2018-02-05 18:49:08 +0000

    updates

--- js/numbers.js
+++ js/numbers.js
@@ -1,59 +1,48 @@
 (function() {
 
-	var
-	base = 2,
-	multiplier = 1,
-	multiply = function() {
-		return base*multiplier;
-	},
-	sum = function() {
-		return [base, multiplier].join(' x ');
-	},
-	replacer = function(string, replacers) {
-		var out = string;
-		for(var replacer in replacers) {
-			out = out.replace('{' + replacer + '}', replacers[replacer]);
-		};
-		return out;
-	},
-	render = function() {
-		target.innerHTML = replacer(template, {
-			question: sum(),
-			answer: multiply()
-		});
-		document.body.setAttribute('data-colour', colours[colour]);
-	},
-	target = document.getElementById('app'),
-	template = '<div id="question">{question}</div><div id="answer">{answer}</div>',
-	colours = [
-		'red',
-		'orange',
-		'yellow',
-		'green',
-		'blue',
-		'indigo',
-		'violet'
-	],
-	colour = 0;
+	window.numbers = function(operation) {
 
-	document.body.addEventListener('mousedown', function(e) {
+		var
+		base = 1,
+		multiplier = 1,
+		calculate = function() {
+			return operations[operation].calculate(base, multiplier);
+		},
+		sum = function() {
+			return [base, multiplier].join(' ' + operations[operation].value + ' ');
+		},
+		replacer = function(string, replacers) {
+			var out = string;
+			for(var replacer in replacers) {
+				out = out.replace('{' + replacer + '}', replacers[replacer]);
+			};
+			return out;
+		},
+		render = function() {
+			target.innerHTML = replacer(template, {
+				question: sum(),
+				answer: calculate()
+			});
+			document.body.setAttribute('data-colour', colours[colour]);
+		},
+		createSelect = function(items) {
 
-		e.preventDefault();
+			var
+			out = document.createElement('select');
 
-	});
+			items.forEach(function(item, index) {
 
-	document.body.addEventListener('click', function(e) {
+				var option = document.createElement('option');
+				option.innerHTML = item.label;
+				option.value = index;
+				out.appendChild(option);
 
-		if(e.target.id==='question') {
+			});
 
-			base ++;
-			multiplier = 1;
-			colour = 0;
+			return out;
 
-		}
-		else {
-
-			multiplier ++;
+		},
+		incrementColour = function() {
 
 			if(colour===colours.length-1) {
 				colour = 0;
@@ -62,12 +51,89 @@
 				colour ++;
 			};
 
+		},
+		target = document.getElementById('app'),
+		template = '<div id="question">{question}</div><div id="answer">{answer}</div><div id="base-up"></div><div id="base-down"></div><div id="multiplier-up"></div><div id="multiplier-down"></div>',
+		colours = [
+			'red',
+			'orange',
+			'yellow',
+			'green',
+			'blue',
+			'indigo',
+			'violet'
+		],
+		colour = 0,
+		operations = {
+			multiply: {
+				label: 'multiply',
+				value: 'x',
+				calculate: function(a, b) {
+					return a*b;
+				}
+			},
+			divide: {
+				label: 'divide',
+				value: '/',
+				calculate: function(a, b) {
+					return a/b;
+				}
+			},
+			add: {
+				label: 'add',
+				value: '+',
+				calculate: function(a, b) {
+					return a+b;
+				}
+			},
+			subtract: {
+				label: 'subtract',
+				value: '-',
+				calculate: function(a, b) {
+					return a-b;
+				}
+			}
 		};
+
+		target.addEventListener('mousedown', function(e) {
+
+			e.preventDefault();
+
+		});
+
+		target.addEventListener('click', function(e) {
+
+			switch(e.target.id) {
+				case 'multiplier-up':
+
+					multiplier ++;
+
+				break;
+				case 'multiplier-down':
+
+					multiplier --;
+
+				break;
+				case 'base-up':
+
+					base ++;
+
+				break;
+				case 'base-down':
+
+					base --;
+
+				break;
+			}
+
+			incrementColour();
+
+			render();
+
+		});
 
 		render();
 
-	});
-
-	render();
+	};
 
 })();
